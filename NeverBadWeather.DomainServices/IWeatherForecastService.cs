76498bda6f1eaf7531827ab2d4f5798ac31222e7
GET Academy <terje@kolderup.net>
2020-06-24 11:33:47 +0200

    x

---
+++ NeverBadWeather.DomainServices/IWeatherForecastService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NeverBadWeather.DomainModel;
+
+namespace NeverBadWeather.DomainServices
+{
+    public interface IWeatherForecastService
+    {
+        WeatherForecast GetWeatherForecast(Location location);
+    }
+}
---
+++ NeverBadWeather.Infrastructure.WeatherForecastService/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NeverBadWeather.Infrastructure.WeatherForecastService
+{
+    public class Class1
+    {
+    }
+}
