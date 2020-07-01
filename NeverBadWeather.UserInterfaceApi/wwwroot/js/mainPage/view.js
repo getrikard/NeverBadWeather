9bed928198b62425bdde794c3bc1b4e6dca22da1
GET Academy <terje@kolderup.net>
2020-07-01 10:20:46 +0200

    Update view.js

--- NeverBadWeather.UserInterfaceApi/wwwroot/js/mainPage/view.js
+++ NeverBadWeather.UserInterfaceApi/wwwroot/js/mainPage/view.js
@@ -3,6 +3,8 @@
         <h3>Aldri dårlig vær!</h3>
 
         Hvilken dag vil du ha klesråd for? <br/>
-        <input type="date" oninput="appContext.model.date = this.value"/>
+        <input type="date" oninput="appContext.model.date = this.value"/><br/>
+        Hvilken tidsperiode 
+
     `;
 });
